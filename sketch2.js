f49cd7b71a91157b542ba101dfc416dd7f01b82b
JustynnePetty <jrpetty3@gmail.com>
2025-04-30 01:01:08 -0500

    final?

---
+++ sketch2.js
@@ -0,0 +1,23 @@
+let bgMusic;
+let isPlaying = false;
+
+function preload() {
+  bgMusic = loadSound("Come-hither please.mp3");
+}
+
+function setup() {
+  bgMusic.loop(); 
+  bgMusic.setVolume(0.5);
+}
+
+function setup() {
+    createCanvas(1300, 2000);
+}
+
+function mousePressed() {
+  if(bgMusic.isPlaying()) {
+    bgMusic.stop();
+  } else {
+    bgMusic.play();
+  }
+}
